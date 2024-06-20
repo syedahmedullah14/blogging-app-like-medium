ba48b3e5fae87da6c1e90e8429f9d15042b7912e
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-20 13:09:03 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+export default app
---
+++ backend/steps.md
@@ -0,0 +1,12 @@
+### Make a root directory
+
+## mkdir backend and intialize a hono project
+
+## make two folder in the root folder i.e backend and frontend
+
+## add common routes in src/index.ts
+
+## get database url and connection pool url from neon and prisma accelerate respectively.
+
+# put connection pool url in wrangler.toml and main database url in .env
+
