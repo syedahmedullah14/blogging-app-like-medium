2a398c3aa9b44ad88d2fb37302b37ddd33c8b537
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-22 22:38:33 +0530

    Implemented auth part

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, ChangeEventHandler, useState } from "react"
 import { Link } from "react-router-dom"
 export const Auth = ({type}: {type: "signup" | "signin"}) => {
 
-  const postInputs = useState<SignupInput>({
+  const [postInputs, setPostInputs] = useState<SignupInput>({
     name: "",
     username: "",
     password: ""
@@ -16,28 +16,53 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
           <div className="text-4xl font-extrabold">
                 Create an account
             </div>
-            <div className="text-slate-500">
+            <div className="text-slate-500 mt-3 mb-3">
                 Already have an account?
                 <Link className="pl-2 underline" to={"/signin"}>Login</Link>
             </div>
+
+            <LabelledInput label="Name" placeholder="Syed Jaser" onChange={(e) =>{
+              setPostInputs({
+                ...postInputs,
+                name: e.target.value
+              })
+            }}/>
+
+            <LabelledInput label="Username" placeholder="syedjaser" onChange={(e) =>{
+              setPostInputs({
+                ...postInputs,
+                name: e.target.value
+              })
+            }}/>
+
+            <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) =>{
+              setPostInputs({
+                ...postInputs,
+                name: e.target.value
+              })
+            }}/>
+
+          <button type="button" className="mt-2 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Sign In"}</button>
+
           </div>
-            
         </div>
     </div>
+    
   )
 }
 
 interface LabelledInputType{
   label: string,
   placeholder: string,
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string
 }
 
-function LabelledInput({label, placeholder, onChange}: LabelledInputType){
+function LabelledInput({label, placeholder, onChange, type}: LabelledInputType){
   return <div>
 
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-      <input onChange={onChange} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+      <label className="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">{label}</label>
+      <input onChange={onChange} type={type || "text"} id="first_name" className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
         
         </div>
 }
