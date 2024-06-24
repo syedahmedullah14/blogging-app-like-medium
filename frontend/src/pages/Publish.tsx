42e801a9d12d3a6e77638ecf57cce5e34a99d3a8
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-24 22:32:24 +0530

    Implemented blog puchising logic

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -1,11 +1,14 @@
 import axios from "axios"
 import { Appbar } from "../components/Appbar"
 import { BACKEND_URL } from "../config"
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export const Publish = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const navigate = useNavigate()
+
   return (
     <div><Appbar />
         <div className="flex justify-center w-full pt-8">
@@ -15,12 +18,20 @@ export const Publish = () => {
                     setTitle(e.target.value)
                 }} type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Title">
                 </input>
-                <TextEditor />
-                <button onClick={() => {
-                    axios.post(`${BACKEND_URL}/api/v1/blog`, {
+                <TextEditor onChange={(e) => {
+                    setDescription(e.target.value)
+                }} />
+                <button onClick={async () => {
+                    const response = await axios.post(`${BACKEND_URL}/api/v1/blog`, {
                         title,
-                        description
-                    })
+                        content: description
+                        
+                    }, {
+                        headers: {
+                            Authorization: localStorage.getItem("token")
+                        }
+                    });
+                    navigate(`/blog/${response.data.id}`)
                     }} type="submit" className="mt-4 inline-flex items-center px-3 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
                     Publish post
                 </button> 
@@ -31,7 +42,7 @@ export const Publish = () => {
   )
 }
 
-function TextEditor(){
+function TextEditor({ onChange }: { onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void}){
     return(
 <div className="mt-8">
    <div className="w-full mb-4">
@@ -39,7 +50,7 @@ function TextEditor(){
        
         <div className="my-2 bg-white rounded-b-lg w-full">
             <label  className="sr-only">Publish post</label>
-            <textarea id="editor" rows={8} className="block w-full px-0 text-sm focus-outline-none text-gray-800 bg-white border-0  " placeholder="Write a blog..." required ></textarea>
+            <textarea onChange={onChange} id="editor" rows={8} className="block w-full px-0 text-sm focus-outline-none text-gray-800 bg-white border-0  " placeholder="Write a blog..." required ></textarea>
         </div>
    </div>
    
