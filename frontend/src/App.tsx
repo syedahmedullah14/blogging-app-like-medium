bbed12ab06c6d6affe88b83ba8cf5a5c049e025d
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-25 17:20:31 +0530

    resolved avatar icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/" element={<Blogs />} />
+          <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -54,10 +54,11 @@ export function Circle(){
     </div>
 }
 
-export function Avatar({ name, size=6 }: { name: string, size:Number}){
+export function Avatar({ name, size= "small"}: { name: string, size: "small" | "big"}){
     return <div className={`relative inline-flex items-center justify-center 
-    w-${size} h-${size} overflow-hidden bg-gray-400 rounded-full dark:bg-gray-700`}>
-        <span className="font-small text-gray-900 dark:text-gray-300">
+    overflow-hidden bg-gray-400 rounded-full ${size === "small" ? "w-6 h-6": "w-10 h-10"}`}>
+        
+        <span className={`${size === "small" ? "text-xs" : "text-md"} font-small text-gray-900 dark:text-gray-300`}>
             {name[0]}
         </span>
     </div>
