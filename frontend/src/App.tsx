2efc67c30a13f1465f1f0fc34a4a4bee72224d33
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-25 14:54:00 +0530

    changes blogs route to "/"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
